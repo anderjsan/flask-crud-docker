a005e8c83dde1b4d7006798bea527047f721559f
Anderson J. Santos <anderjsan@gmail.com>
2024-05-13 11:27:14 -0300

    [FEATURE] Controle de Versao

--- version-control.py
+++ version-control.py
@@ -1,38 +1,126 @@
 import subprocess as sp
+import json
+class CommitUpdater:
+    def __init__(self) -> None:
+        self.commit_keywords = {
+            "[RELEASE]": 0,
+            "[FEATURE]": 1,
+            "[FIX]": 2,
+            "[INIT]": 2  # Assuming [INIT] should increment the FIX term
+        }
+        self.version_terms = [0,0,0]
+        self.last_tag = [0,0,1]
+        self.new_tag = [0,0,1]
+        self.keyword = ""
+        self.commit_message = ""
+        
+    def get_curr_version(self):
+        # get the last version
+        with open(".version", "r") as f:
+            last_version = f.readline().strip()
+        
+        last_version_parts = last_version.split(".")
+        self.last_tag = last_version_parts
+        return last_version_parts
 
-cmt = False
-commit_keywords = {
-    "[RELEASE]": 0,
-    "[FEATURE]": 1,
-    "[FIX]": 2,
-    "[INIT]": 2  # Supondo que [INIT] deve incrementar o termo de correção
-}
+        
+    def set_version_terms(self):
+        # capture the last commit.
+        self.commit_message = sp.run(["git", "log", "-1", "--pretty=%B"], capture_output=True, text=True).stdout.strip()
+        
+        # get the commit type
+        for keyword, index in self.commit_keywords.items():
+            if keyword in self.commit_message:
+                self.version_terms[index] += 1
+                self.keyword = keyword
+                break
 
-version_terms = [0, 0, 0]
+    def get_new_version(self, tag):
+        parts = tag
+        if self.version_terms[0] > 0:
+            parts[0] = str(int(parts[0]) + self.version_terms[0])
+            parts[1] = '0'
+            parts[2] = '0'
+        elif self.version_terms[1] > 0:
+            parts[1] = str(int(parts[1]) + self.version_terms[1])
+            parts[2] = '0'
+        elif self.version_terms[2] > 0:
+            parts[2] = str(int(parts[2]) + self.version_terms[2])
+        return parts
+    
+    def set_new_version(self, tag):
+        # aplicar o calculo da nova versão:
+        # Increment version
+        new_tag = tag
+        if x.version_terms[0] > 0:
+            new_tag[0] = str(int(new_tag[0]) + x.version_terms[0])
+            new_tag[1] = '0'
+            new_tag[2] = '0'
+        elif x.version_terms[1] > 0:
+            new_tag[1] = str(int(new_tag[1]) + x.version_terms[1])
+            new_tag[2] = '0'
+        elif x.version_terms[2] > 0:
+            new_tag[2] = str(int(new_tag[2]) + x.version_terms[2])
 
-# capture the ongoing commit message
-last_commit = sp.run(["git", "log", "-1", "--pretty=%B"], capture_output=True, text=True).stdout.strip()
-print(last_commit)
-# check if the commit message contains the tag
-cmt = False
-for keyword, index in commit_keywords.items():
-    if keyword in last_commit:
-        version_terms[index] += 1
-        cmt = True
-        break
+
+        new_version = ".".join(new_tag)
+        with open(".version", "w") as f:
+            f.write(new_version)
+        self.new_tag = new_tag
+        return new_version
+
+    
+    def log_verson(self, tag, keyword, message):
+        commit_log = []
+        # open version_log and feed an array of commits.
+        with open(".version_log", "r") as json_file:
+            commit_logs = json.load(json_file)
+        
+        #prepare a new commit_log to enter into commit_log list.
+        if keyword == "[FIX]":
+            commit_log = {
+                "tag": tag,
+                "git_message": f"{message}"
+            }
+        else:
+            commit_log = {
+                "tag": tag,
+                "git_message": f"{message}",
+                "fixes": []
+            }
+        
+        #if keyword means FIX, search for the FEATURE or release tag where this fix was belongs to. Add this commit_log into the commit_logs list under FIXES, respecting the parenthood
+        if keyword == "[FIX]":
+            for commit in reversed(commit_logs):
+                # se os dois primeiros termos da tag são iguais, faça o append do commit_log.
+                # Separando os dois primeiros termos das tags
+                existing_tag_terms = commit["tag"].split(".")[:2]
+                new_tag_terms = tag[:2]
+
+                # Verificando se os dois primeiros termos são iguais
+                if existing_tag_terms == new_tag_terms:
+                    commit["fixes"].append(commit_log)
+                    break
+        else:
+            commit_logs.append(commit_log)
+        
+        #save commit_logs into version_log file
+        with open(".version_log", "w") as json_file:
+            json.dump(commit_logs, json_file)
+
+        print(commit_logs)
+                
+                
+
+        #if not, add a new commit_log to the commit_logs list, with FIXES as an empty inner list
+
     
 
-#read the version file
-with open(".version", "r") as f:
-    current_version = f.readline().strip()
-    print(current_version)
-
-#increments the version according to the tag
-current_version_parts = current_version.split(".")
-current_version_parts[0] = str(int(current_version_parts[0]) + version_terms[0])
-current_version_parts[1] = str(int(current_version_parts[1]) + version_terms[1])
-current_version_parts[2] = str(int(current_version_parts[2]) + version_terms[2])
-
-#updates the version file
-with open(".version", "w") as f:
-    f.write(".".join(current_version_parts))
+x = CommitUpdater()
+x.set_version_terms()
+vers = x.get_curr_version()
+tag = x.set_new_version(vers)
+x.log_verson(tag,x.keyword,x.commit_message)
+# print(vers)
+# print(x.set_new_version(vers))
+x.log_verson
