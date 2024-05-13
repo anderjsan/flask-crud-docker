b11b419c1c40b6ab239af8a5f040003d7a12f635
Anderson J. Santos <anderjsan@gmail.com>
2024-05-12 23:03:46 -0300

    [FEATURE] Controle de Versão

--- version-control.py
+++ version-control.py
@@ -1,28 +1,25 @@
 import subprocess as sp
 
 cmt = False
-release = 0
-feature = 0
-fix = 0
+commit_keywords = {
+    "[RELEASE]": 0,
+    "[FEATURE]": 1,
+    "[FIX]": 2,
+    "[INIT]": 2  # Supondo que [INIT] deve incrementar o termo de correção
+}
+
+version_terms = [0, 0, 0]
 
 # capture the ongoing commit message
 last_commit = sp.run(["git", "log", "-1", "--pretty=%B"], capture_output=True, text=True).stdout.strip()
 print(last_commit)
 # check if the commit message contains the tag
-if "[RELEASE]" in last_commit:
-    release = 1
-    cmt = True
-elif "[FEATURE]" in last_commit:
-    feature = 1
-    cmt = True
-elif "[FIX]" in last_commit:
-    fix = 1
-    cmt = True
-elif "[INIT]" in last_commit:
-    fix = 1
-    cmt = True
-else:
-    cmt = False
+cmt = False
+for keyword, index in commit_keywords.items():
+    if keyword in last_commit:
+        version_terms[index] += 1
+        cmt = True
+        break
     
 
 #read the version file
@@ -32,9 +29,9 @@ with open(".version", "r") as f:
 
 #increments the version according to the tag
 current_version_parts = current_version.split(".")
-current_version_parts[0] = str(int(current_version_parts[0]) + release)
-current_version_parts[1] = str(int(current_version_parts[1]) + feature)
-current_version_parts[2] = str(int(current_version_parts[2]) + fix)
+current_version_parts[0] = str(int(current_version_parts[0]) + version_terms[0])
+current_version_parts[1] = str(int(current_version_parts[1]) + version_terms[1])
+current_version_parts[2] = str(int(current_version_parts[2]) + version_terms[2])
 
 #updates the version file
 with open(".version", "w") as f:
